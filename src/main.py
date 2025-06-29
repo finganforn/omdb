dd18541c5df2b91139cb2285bba403578f470941
Erik <erik.hahne@gmail.com>
2025-06-29 14:19:02 +0200

    get endpoints reimplemented

--- src/main.py
+++ src/main.py
@@ -12,6 +12,8 @@ from peewee import *
 db = SqliteDatabase('movies.db')
 db.connect()
 
+initialMoviesAmount = 25
+
 class Movie(Model):
     title = CharField()
     year = IntegerField()
@@ -21,12 +23,8 @@ class Movie(Model):
     class Meta:
         database = db
 
-
-
-if db.get_tables() == []:  # If there are no tables in the database
-    print("Creating database and tables...")
-    db.create_tables([Movie])
-    initialMovies = fetch_random_movies(10)  # Fetch initial random movies from OMDB API
+def fillDB(amount):
+    initialMovies = fetch_random_movies(amount)  # Fetch initial random movies from OMDB API
     for movie in initialMovies:
         strYear = movie.get('Year', '0')
         #if strYear contains - remove after the dash
@@ -34,6 +32,8 @@ if db.get_tables() == []:  # If there are no tables in the database
             strYear = strYear.split('-')[0]
         if '–' in strYear:
             strYear = strYear.split('–')[0]
+        if not strYear.isdigit():
+            strYear = '0'
         try:
             Movie.create(
                 title=movie.get('Title', 'N/A'),
@@ -43,30 +43,64 @@ if db.get_tables() == []:  # If there are no tables in the database
             )
         except IntegrityError:
             print(f"Movie with IMDB ID {movie.get('imdbID')} already exists.")
+
+if db.get_tables() == []:  # If there are no tables in the database
+    print("Creating database and tables...")
+    db.create_tables([Movie])
+    fillDB(initialMoviesAmount)
+    
 else:
     print("Database already exists, skipping creation.")
     #get size of Movie table
-    print(f"Number of movies in database: {Movie.select().count()}")
-    for movie in Movie.select():
-        print(movie.title, movie.year, movie.plot)
+    amountMovies = Movie.select().count()
+    print(f"Number of movies in database: {amountMovies}")
+    if (amountMovies < 1):
+        print("No movies found in the database, filling with initial data...")
+        fillDB(initialMoviesAmount)
+        print(f"New number of movies in database: {amountMovies}")
+    
+    #for movie in Movie.select():
+    #    print(movie.title, movie.year, movie.plot)
 
 app = FastAPI()
 
+def getXmovies(limit):
+    query = (Movie
+             .select()
+             .limit(limit)
+             .order_by(Movie.year.desc()))
+    list = []
+    for movie in query:
+        list.append({
+            "Title": movie.title,
+            "Year": movie.year,
+            "Plot": movie.plot,
+            "imdbID": movie.imdb_id
+        })
+    return list
+
 @app.get("/") # Root endpoint 
 def get10(): #will return 10 by default
-    return movies[:10]
+    return getXmovies(10)
+    
 
 @app.get("/limit/{limit}")
 def getSpecificNum(limit: int):
-    return movies[:limit]
+    return getXmovies(limit)
 
 @app.get("/item/{item_title}")
 def read_item(item_title: str):
-    print(f"Searching for item: {item_title}")
-    for movie in movies:
-        if movie.get('Title', '').lower() == item_title.lower():
-            return movie
-    return {"error": "Item not found"}
+    #get by title
+    try:
+        movie = Movie.get(Movie.title == item_title)
+        return {
+            "Title": movie.title,
+            "Year": movie.year,
+            "Plot": movie.plot,
+            "imdbID": movie.imdb_id
+        }  
+    except Movie.DoesNotExist:
+        return {"error": "Item not found"}
 
 @app.post("/item/")
 def create_item(item):
